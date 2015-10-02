b6dd90e226b6ce88c3cbe997fa5570f4195bc880
ANIVRA <antonio.raynor@gmail.com>
2015-10-02 13:41:10 -0400

    .Net Authentication designed in AngularJS
    AnjularJS application template with initial .Net login/register
    functionality.

---
+++ AngularSecurity/Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AngularSecurity.Areas.HelpPage.ModelDescriptions
+{
+    public class ParameterAnnotation
+    {
+        public Attribute AnnotationAttribute { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}
---
+++ AngularSecurity/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace AngularSecurity.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
